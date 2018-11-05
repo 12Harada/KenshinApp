7529cbbb828545dc5ef98abc379264484b4bc8b3
12harada <12haradaut@gmail.com>
2018-11-05 18:08:21 +0900

    Initial Commit

---
+++ KenshinApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KenshinApp
+//
+//  Created by Harada Hiroaki on 2018/11/05.
+//  Copyright © 2018 Harada Hiroaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

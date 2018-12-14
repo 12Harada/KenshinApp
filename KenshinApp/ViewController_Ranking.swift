fb81de550d0efcccd66241bfddf1b009f9b1b910
12harada <12haradaut@gmail.com>
2018-12-14 21:47:10 +0900

    2018/12/14 原田

---
+++ KenshinApp/ViewController_Ranking.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController_Ra mj g.swift
+//  KenshinApp
+//
+//  Created by Harada Hiroaki on 2018/12/14.
+//  Copyright © 2018 TomonariNonaka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController_Ranking: UIViewController {
+    
+    var motion = [Motion]()
+    
+    
+    
+    override func viewDidLoad() {
+    self.navigationItem.title = "ランキング"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+}
